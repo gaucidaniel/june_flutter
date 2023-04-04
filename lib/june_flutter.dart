cadc03d557a09fe5454584ffb8ab496382240002
Daniel Gauci <daniel.gauci@gmail.com>
2023-04-04 21:54:46 +0200

    Define June API

--- lib/june_flutter.dart
+++ lib/june_flutter.dart
@@ -1,3 +1,27 @@
 library june_flutter;
 
-class June {}
+class June {
+  void init({required String writeKey}) async {}
+
+  Future<bool> identifyUser({
+    String? userId,
+    Map<String, dynamic> traits = const {},
+  }) async {
+    return true;
+  }
+
+  Future<bool> identifyGroup({
+    String? userId,
+    String? groupId,
+    Map<String, dynamic> traits = const {},
+  }) async {
+    return true;
+  }
+
+  Future<bool> track(
+    String event, {
+    Map<String, dynamic>? properties,
+  }) async {
+    return true;
+  }
+}
